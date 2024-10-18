e3077c25bdadff8c0003af02ec81f3e2b05f5f5a
Pruthvik <pruthvik6666@gmail.com>
2024-10-18 11:42:10 +0530

    init

--- week-4/hard/database/index.js
+++ week-4/hard/database/index.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(MONGODB_URI );
 
 // Define schemas
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    userName:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+}
 });
 
 const TodoSchema = new mongoose.Schema({
     // Schema definition here
+    task:{
+        type: String,
+        required: true
+    },
+    completed:{
+        type: Boolean,
+        default: false
+    },
 });
 
 const User = mongoose.model('User', UserSchema);
