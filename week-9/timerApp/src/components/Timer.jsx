f9a5fa908e15315223be7361bac15a70f56b28e7
Pruthvik <pruthvik6666@gmail.com>
2024-11-04 21:10:39 +0530

    week 9

--- week-9/timerApp/src/components/Timer.jsx
+++ week-9/timerApp/src/components/Timer.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {useState, useEffect } from "react";
+import style from "./Timer.module.css";
+import { formatTime, calculateTime } from "../utils/auxiliaryFunctions";
 
 const Timer = () => {
-  return (
-    <div>Timer</div>
-  )
+  const [time, setTime] = useState(0);
+  const [initialTime, setInitialTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [editState, setEditState] = useState(false);
+
+  useEffect(()=>{
+    const progress = setInterval > 0 ? ((initialTime - time) / initialTime) * 100 : 0
+    document.documentElement.style.setProperty('--progress', `${progress}%`);
+  }, [time, initialTime]);
+
+  useEffect(()=>{
+    let interval = null;
+    if(isRunning && time > 0){
+      interval = setInterval(()=>{
+        setTime((prevTime) => time - 1);
+      },1000);
+    } else if(time === 0){
+      setIsRunning(false);
+    }
+    return () =>{
+      if(interval) clearInterval(interval);
+    }
+  }, [isRunning, time]);
+
 }
 
+ 
 export default Timer
